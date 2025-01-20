07893a413e64a1ffc669c105f8cec1ce149e4994
Ashen1438 <asheneranga1438@gmail.com>
2025-01-21 02:32:26 +0530

    Add Firestore integration to signup form and profile screen, update user registration to store additional user details

--- lib/screens/user_panel/profile_screen.dart
+++ lib/screens/user_panel/profile_screen.dart
@@ -1,5 +1,6 @@
 import 'package:agri_mart/constants/bg_ellipse.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ProfileScreen extends StatelessWidget {
   @override
